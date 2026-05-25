d77bba56d62c410a2f4131c1d80d7ea3ecaeeba4
Tanvir1577 <onlinehelp2007@gmail.com>
2026-05-25 20:52:47 +0600

    add env vars, .env.example for Vercel

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB-ysWC9I5CBOTjMFcKd2q2l68_NLVbEzU",
-  authDomain: "cineverse-bc951.firebaseapp.com",
-  projectId: "cineverse-bc951",
-  storageBucket: "cineverse-bc951.firebasestorage.app",
-  messagingSenderId: "838134026814",
-  appId: "1:838134026814:web:4dd1a6e401a4296974776f",
-  measurementId: "G-MHMPKW6529"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
